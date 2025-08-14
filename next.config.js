e28fea61465f4e30d853f7a0b7e0d3df8b070632
Vornvoskhq <msudick02@gmail.com>
2025-08-14 22:48:19 +0000

    chore: update shadcn-ui version and clean up config files
    Co-authored-by: Genie <genie@cosine.sh>

--- next.config.js
+++ next.config.js
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-  experimental: {
-    appDir: false
-  }
+  reactStrictMode: true
 };
 
 module.exports = nextConfig;
